ac1b0d57e0f5a7c57eaff05a61bede15377de047
shimshir <me.admir@gmail.com>
2015-08-11 00:45:24 +0200

    Added post-date component and implemented embedding youtube videos

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -6,11 +6,24 @@ function mainController($scope, SocialService) {
 		$scope.comments= {};
 		$scope.comments.data= {};
 		
-	  	SocialService.getAllPosts().then(function (response) {
-            $scope.posts=response.data;
-         });
+		SocialService.getAllPosts().then(function (response) {
+			$scope.posts=response.data;
+		});
 
-         $scope.closeComments = function() {
-         	$('#commentModal').modal('hide');
-         }
+		$scope.closeComments = function() {
+			$('#commentModal').modal('hide');
+		}
+		$scope.getFormattedPostDate = function(unixTimestamp) {
+			var date = new Date(unixTimestamp * 1000);
+			var day = date.getDate();
+			var month = date.getMonth();
+			var year = date.getFullYear();
+			var hours = date.getHours();
+			var minutes = ("0" + date.getMinutes()).substr(-2);
+			var seconds = ("0" + date.getSeconds()).substr(-2);
+
+			var formattedTime = date.getDate() + "." + date.getMonth() + "." + year + 
+				", " + hours + ':' + minutes + ':' + seconds;
+			return formattedTime;
+		}
 }
